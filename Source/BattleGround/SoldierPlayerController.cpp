cf9ff451be490a54016c26afc22729ae5cff7acc
Mohammed Abushawish <53370197+mkcit@users.noreply.github.com>
2020-08-08 21:43:16 +0300

    Update

--- Source/BattleGround/SoldierPlayerController.cpp
+++ Source/BattleGround/SoldierPlayerController.cpp
@@ -1,5 +1,31 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 
 #include "SoldierPlayerController.h"
 
+void ASoldierPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ShowWidgetOnScreen(GunWidget, GunWidgetClass, false);
+}
+
+void ASoldierPlayerController::RemoveAllWidgets()
+{
+	if (GunWidget) 
+	{
+		if (GunWidget->IsInViewport())
+			GunWidget->RemoveFromViewport();
+	}
+
+}
+
+void ASoldierPlayerController::ShowWidgetOnScreen(UUserWidget* Widget, TSubclassOf<UUserWidget> WidgetClass, bool SavePreviousWidget)
+{
+	if (!SavePreviousWidget)
+		RemoveAllWidgets();
+
+	Widget = CreateWidget(this, WidgetClass);
+	if (Widget)
+	{
+		Widget->AddToViewport();
+	}
+}
--- Source/BattleGround/SoldierPlayerController.h
+++ Source/BattleGround/SoldierPlayerController.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Blueprint/UserWidget.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "SoldierPlayerController.generated.h"
@@ -13,5 +14,21 @@ UCLASS()
 class BATTLEGROUND_API ASoldierPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void RemoveAllWidgets();
+
+	void ShowWidgetOnScreen(UUserWidget* Widget, TSubclassOf<UUserWidget> WidgetClass, bool SavePreviousWidget);
+
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> GunWidgetClass;
+
+	UUserWidget* GunWidget = nullptr;
+
+
 	
 };
